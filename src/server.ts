417f3cd5ef61f0a526756e9168f358e6b4f27927
brunocordioli072 <brunocordioli072@gmail.com>
2020-11-01 20:54:16 -0300

    new things

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-const isDev = process.env.NODE_ENV != "prod";
-if (isDev) {
-  process.env.VIEW_URL = process.env.VIEW_URL_DEV;
-  process.env.REDIRECT_URI = `${process.env.API_URL_DEV}/auth/spotify/callback`;
-} else {
-  process.env.VIEW_URL = process.env.VIEW_URL_PROD;
-  process.env.REDIRECT_URI = `${process.env.API_URL_PROD}/auth/spotify/callback`;
-}
+process.env.VIEW_URL = "http://localhost:3000";
+process.env.REDIRECT_URI = `http://localhost:8080/local/auth/spotify/callback`;
 
 const app = express();
 
@@ -37,20 +31,18 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
   next(err);
 });
 
-if (isDev) {
-  app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-    console.log(err.stack);
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  console.log(err.stack);
 
-    res.status(err.status || 500);
+  res.status(err.status || 500);
 
-    res.json({
-      errors: {
-        message: err.message,
-        error: err,
-      },
-    });
+  res.json({
+    errors: {
+      message: err.message,
+      error: err,
+    },
   });
-}
+});
 
 app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
   res.status(err.status || 500);
